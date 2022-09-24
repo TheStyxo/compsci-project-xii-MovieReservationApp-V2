737c266e133a952972e2c59b3183c3f4618dcdc9
Paras Deshpande <parasbpndeshpande@gmail.com>
2022-09-25 00:22:15 +0530

    final

--- codes/styxo/school/projects/SkyCinemasV2/App.java
+++ codes/styxo/school/projects/SkyCinemasV2/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("demo")) {
             demoMode = true;
-            //DataStore.replaceWithSampleData();
+            DataStore.replaceWithSampleData();
         }
         Menu.main();
     }
